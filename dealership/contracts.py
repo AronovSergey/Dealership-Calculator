7d554db98702f66db63ec8820bf45fa4ca351951
AronovSergey <43394177+AronovSergey@users.noreply.github.com>
2019-04-24 21:45:14 +0300

    Removed duplicated code

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -14,16 +14,14 @@ class BuyContract(Contract):
         self.monthly_payments = monthly_payments
     
     def total_value(self):
-        
-        
-        
-        vehicle_multiplier = 0
-        if isinstance (self.vehicle, Car):
-            vehicle_multiplier = 1.07
-        elif isinstance (self.vehicle, Motorcycle):
-            vehicle_multiplier = 1.03
-        elif isinstance (self.vehicle, Truck):
-            vehicle_multiplier = 1.11
+
+        VEHICLE_MULTIPLIERS = {
+            Car: 1.07,
+            Motorcycle: 1.03,
+            Truck: 1.11
+        }
+        vehicle_multiplier = VEHICLE_MULTIPLIERS[self.vehicle.__class__]
+
         price = self.vehicle.sale_price() + (vehicle_multiplier * self.monthly_payments * self.vehicle.sale_price() / 100)
         if isinstance (self.customer, Employee):
             return price - (price * .1)
@@ -41,19 +39,20 @@ class LeaseContract(Contract):
         self.length_in_months = length_in_months
     
     def total_value(self):
-        lease_multiplier = 0
-        if isinstance(self.vehicle, Car):
-            lease_multiplier = self.vehicle.sale_price() * 1.2/self.length_in_months
-        elif isinstance(self.vehicle, Motorcycle):
-            lease_multiplier = self.vehicle.sale_price() * 1/self.length_in_months
-        elif isinstance(self.vehicle, Truck):
-            lease_multiplier = self.vehicle.sale_price() * 1.7/self.length_in_months
+        VEHICLE_MULTIPLIERS = {
+            Car: 1.2,
+            Motorcycle: 1,
+            Truck: 1.7
+        }
+        vehicle_multiplier = VEHICLE_MULTIPLIERS[self.vehicle.__class__]
+
+        lease_multiplier = self.vehicle.sale_price() * vehicle_multiplier/self.length_in_months
         price = self.vehicle.sale_price() + lease_multiplier
         if isinstance (self.customer, Employee):
             return price - (price * .1)
         return price
 
     def monthly_value(self):
-        return self.total_value() / self.length_in_months 
+        return self.total_value() / self.length_in_months
 
 
